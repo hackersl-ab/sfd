64196df1966f9dda28da6fdbb9c938918d0aeebb
haha Che <test@test.com>
2020-10-07 20:40:52 -0400

    d

--- gas.py
+++ gas.py
@@ -1,6 +1,12 @@
-print("lol")
-print("oaisdjoidj")
-print('doss')
+class human:
+    def __init__(self):
+        self.fat = 10
+        self.weight = self.fat * 69
 
-print('dosiajodsfi')
-#aoisjoijasdoifsdoifsd
+    def heavy(self):
+        print("He/she weighs", self.weight, "pounds")
+
+Pranav = human()
+
+Pranav.fat += 69
+Pranav.heavy()
